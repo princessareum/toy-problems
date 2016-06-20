2f703dae806ee1db2219d4763c9dcadbf703d5e2
A-Reum Jung <areumjung1987@gmail.com>
2016-06-20 16:40:52 -0600

    day 1

---
+++ javascript/alphabet.js
@@ -0,0 +1,15 @@
+// write a functin that takes in a string, and returns that string with all
+// its letters in alphabetal order. assume there will be no punctuation or
+// numbers. example: `alphabet('hello')` // => 'ehllo'
+
+
+
+function alphabet(str){
+  return str.split('').sort().join('');
+};
+
+
+
+//test
+const string = "hello";
+console.log(alphabet(string));
